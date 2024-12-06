f1233a4d69c823e29a6405952b01831c50be6e66
heryogurt <92820593+heryogurt@users.noreply.github.com>
2024-12-07 00:47:25 +0300

    Add files via upload

---
+++ cmd/count/main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "$41yohul22"
+	dbname   = "web_8"
+)
+
+type Handlers struct {
+	dbProvider DatabaseProvider
+}
+
+type DatabaseProvider struct {
+	db *sql.DB
+}
+
+// Обработчики HTTP-запросов
+func (h *Handlers) GetHello(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		msg, err := h.dbProvider.SelectHello()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(strconv.Itoa(msg)))
+	} else {
+		h.PostHello(w, r)
+	}
+}
+func (h *Handlers) PostHello(w http.ResponseWriter, r *http.Request) {
+	input := struct {
+		Count int `json:"count"`
+	}{}
+
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&input)
+	if err != nil {
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+		}
+	}
+
+	err = h.dbProvider.InsertHello(input.Count)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+	}
+	w.WriteHeader(http.StatusCreated)
+}
+
+// Методы для работы с базой данных
+func (dp *DatabaseProvider) SelectHello() (int, error) {
+	var msg int
+
+	// Получаем одно сообщение из таблицы hello, отсортированной в случайном порядке
+	row := dp.db.QueryRow("SELECT number FROM count")
+	err := row.Scan(&msg)
+	fmt.Println("from get: ", msg)
+	if err != nil {
+		return 0, nil
+	}
+	return msg, nil
+}
+func (dp *DatabaseProvider) InsertHello(msg int) error {
+	fmt.Println(msg)
+	_, err := dp.db.Exec("UPDATE count SET number = number + $1", msg)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	// Считываем аргументы командной строки
+	address := flag.String("address", "127.0.0.1:8081", "адрес для запуска сервера")
+	flag.Parse()
+
+	// Формирование строки подключения для postgres
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	// Создание соединения с сервером postgres
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Создаем провайдер для БД с набором методов
+	dp := DatabaseProvider{db: db}
+	// Создаем экземпляр структуры с набором обработчиков
+	h := Handlers{dbProvider: dp}
+
+	// Регистрируем обработчики
+	http.HandleFunc("/count", h.GetHello)
+
+	// Запускаем веб-сервер на указанном адресе
+	err = http.ListenAndServe(*address, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
